5301af7150a0187bbbb08be67949c02a7d8711af
Helicose <maxime-la-menace@live.fr>
2017-03-10 12:22:07 +0100

    Page principale et liens (En vues, a faire en activités)

--- app/src/main/java/com/controle_android/chezmomo/MainActivity.java
+++ app/src/main/java/com/controle_android/chezmomo/MainActivity.java
@@ -2,6 +2,11 @@ package com.controle_android.chezmomo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Chez Momo");
+
+        final ListView liste;
+
+        String[] data = new String[]{
+                "Nos Formules","Carte des vins"
+        };
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+
+        liste = (ListView)findViewById(R.id.list);
+        liste.setAdapter(adapter);
+
+        liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    setContentView(R.layout.formules);
+                }else{
+                    if(position==1){
+                        setContentView(R.layout.carte);
+                    }
+                }
+
+            }
+        });
     }
 }
