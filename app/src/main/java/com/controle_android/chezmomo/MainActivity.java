c18cd109aabadd06b3c1d119fa8713acf224b9dc
Falysix <falysix@gmail.com>
2017-03-10 11:10:48 +0100

    Création de la liste de formules

--- app/src/main/java/com/controle_android/chezmomo/MainActivity.java
+++ app/src/main/java/com/controle_android/chezmomo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.controle_android.chezmomo;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.formules);
+        setTitle("Nos formules");
+
+        ListView liste;
+
+        String[] data = new String[]{
+                "Menu Midi", "Menu Gourmand", "Menu Bistrot", "Menu des Petiots"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+
+        liste = (ListView)findViewById(R.id.listView);
+        liste.setAdapter(adapter);
     }
 }
