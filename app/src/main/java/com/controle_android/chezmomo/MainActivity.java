0059ac3d0cda8124c28b3b266f96125dd4079221
Falysix <falysix@gmail.com>
2017-03-13 00:44:39 +0100

    Ajout page choix menu midi

--- app/src/main/java/com/controle_android/chezmomo/MainActivity.java
+++ app/src/main/java/com/controle_android/chezmomo/MainActivity.java
@@ -3,6 +3,7 @@ package com.controle_android.chezmomo;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.app.ListActivity;
@@ -29,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
 
         //liste = (ListView)findViewById(R.id.listView);
         //liste.setAdapter(adapter);
+    }
+
+    public void redirectionFormule(View vue){
+        switch (vue.getId()) {
+            case R.id.cartegourmand:
+                //doSomething1();
+                break;
+            case R.id.cartebistrot:
+                //doSomething2();
+                break;
+            case R.id.cartemidi:
+                setContentView(R.layout.menumidi);
+                setTitle("Choix du Menu Midi");
+                break;
+            case R.id.cartepetiots:
+                //doSomething1();
+                break;
+        }
+
     }
 }
